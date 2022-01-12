9261ebef37492d49ac4be0093bf521092f47562d
nischalbabu <nischal.babu@fibonalabs.com>
2022-01-12 17:58:33 +0530

    refactor: 💡 removing dist from git
    removing dist from git
    
    BREAKING CHANGE: 🧨 No
    
    ✅ Closes: No

--- dist/utils/run.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var child_process_1 = require("child_process");
-var run = function (cmd, args) {
-    return child_process_1.spawnSync(cmd, args, { stdio: "inherit" });
-};
-exports.default = run;
